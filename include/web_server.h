ba95f3cb3c58b64fffb9979556432c4d5fba6f2c
kumarsatyam <kumarsatyam@users.noreply.github.com>
2026-06-19 19:18:09 +0530

    chore: bump version to 2.3.45

--- include/web_server.h
+++ include/web_server.h
@@ -7,7 +7,7 @@
 #include "dashboard_html.h"
 
 #define PART_BOUNDARY "123456789000000000000987654321"
-#define FIRMWARE_VERSION "2.3.44"
+#define FIRMWARE_VERSION "2.3.45"
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 21
 #endif
